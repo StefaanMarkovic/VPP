152e514d64e944deaae27b3c51c640bca0fabe52
Aleksandar <milosevskialeksandar18@gmail.com>
2025-12-02 18:36:24 +0100

    h2

--- src/main/java/mk/finki/ukim/wp/lab/model/BookReservation.java
+++ src/main/java/mk/finki/ukim/wp/lab/model/BookReservation.java
@@ -1,13 +1,28 @@
 package mk.finki.ukim.wp.lab.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@NoArgsConstructor
 @AllArgsConstructor
 public class BookReservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String bookTitle;
     private String readerName;
     private String readerAddress;
     private Long numberOfCopies;
+
+    public BookReservation(String bookTitle, String readerName, String readerAddress, Long numberOfCopies) {
+        this.bookTitle = bookTitle;
+        this.readerName = readerName;
+        this.readerAddress = readerAddress;
+        this.numberOfCopies = numberOfCopies;
+    }
 }
--- src/main/java/mk/finki/ukim/wp/lab/repository/BookReservationRepository.java
+++ src/main/java/mk/finki/ukim/wp/lab/repository/BookReservationRepository.java
@@ -1,7 +1,9 @@
 package mk.finki.ukim.wp.lab.repository;
 
 import mk.finki.ukim.wp.lab.model.BookReservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface BookReservationRepository {
-    BookReservation save(BookReservation reservation);
+@Repository
+public interface BookReservationRepository extends JpaRepository<BookReservation, Long> {
 }
